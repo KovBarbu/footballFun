998564249fd2e4e31ce8421f371ac223ec0d8edd
KovBarbu <kov.barbu@gmail.com>
2017-10-27 17:06:46 +0200

    feat: wip match

--- js/match.js
+++ js/match.js
@@ -1,4 +1,7 @@
+var tableData = [];
+
 var code = location.href.split("=")[1];
+
 getJson(
     "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/en.1.json",
     function (data) {
@@ -10,16 +13,27 @@ getJson(
 function fillTable2(rows) {
     var table = document.querySelector("table");
     var content = "";
+
+
     for (var i = 0; i < rows.length; i++) {
-        var tr = "<tr>";
-        tr += "<td>" + (i + 1) + "</td>";
-        tr += "<td>" + rows[i].name + "</td>";
-        tr += "<td>" + rows[i].code + "</td>";
-        tr += '<td><a class="btn btn-primary" href="match.html?code=' + rows[i].code + '">match</a></td>';
-        tr += "</tr>";
-        content += tr;
+        for (var j = 0; j < rows[i].matches.length; j++) {
+            if (code === matches.team1.code || code === matches.team2.code); {
+                var tr = "<tr>";
+
+                tr += "<td>" + (i + 1) + "</td>";
+                tr += "<td>" + rows[i].matches.date + "</td>";
+                tr += "<td>" + rows[i].matches.team1.name + "</td>";
+                tr += "<td>" + rows[i].matches.team2.name + "</td>";
+                tr += "<td>" + rows[i].matches.score1 + "</td>";
+                tr += "<td>" + rows[i].matches.score2 + "</td>";
+
+                tr += "</tr>";
+                content += tr;
+            }
+        }
+        table.querySelector("tbody").innerHTML = content;
+
     }
 
-    table.querySelector("tbody").innerHTML = content;
-}
+
 }
